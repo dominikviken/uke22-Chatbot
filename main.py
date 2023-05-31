ddf72d454f4df8e5b82809cf85e5f34b502f79f3
dominikviken <dominikse@viken.no>
2023-05-31 14:35:30 +0200

    DU ER MARIHØNE

---
+++ main.py
@@ -0,0 +1,29 @@
+# Importerer tid for spørsmål.
+import time
+
+# Ordbok av Svar
+svar = {
+    # Introduksjoner
+    "god dag"           : "god dag til deg og",
+    "hallo"             : "hallo",
+    
+    # Spørsmål
+    "hvordan er været"  : "vanlig",
+    "hvor gammel er du" : "uendelig år",
+    "hva er klokka"     : f"klokka er {time.strftime('%H:%M')}",
+}
+
+# Uendelig While Loop, for uendelig spørring av spørsmål. 
+while True:
+    # Får input fra Brukeren.
+    userInput = input('> ')
+
+    # Sjekker hvis svaret finnes. Hvis det ikke gjør det, vil AI-en svare med "hæ".
+    if userInput in svar:
+        # Bruker "get" funksjonen til å få verdien av key-en ved å bruke brukeren sin input. Setter det i AIoutput variabelen.
+        AIoutput = svar.get(userInput)
+    else:
+        AIoutput = ('hæ')
+
+    # Printer AI-output
+    print(f'AI: {AIoutput}')
